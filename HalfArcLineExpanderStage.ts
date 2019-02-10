9da6b1d90715cebf7f56cc5965e43fe45493ebfa
Anwesh43 <anweshthecool0@gmail.com>
2019-02-11 01:25:22 +0530

    adding HALENode class

--- HalfArcLineExpanderStage.ts
+++ HalfArcLineExpanderStage.ts
@@ -143,3 +143,48 @@ class Animator {
         }
     }
 }
+
+class HALENode {
+
+    next : HALENode
+    prev : HALENode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new HALENode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawHALENode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : HALENode {
+        var curr : HALENode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr != null) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
