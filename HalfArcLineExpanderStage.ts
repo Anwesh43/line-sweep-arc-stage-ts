0e49532bdd6c07dfa5611e4b20ae32d8e2c7440d
Anwesh43 <anweshthecool0@gmail.com>
2019-02-10 19:21:44 +0530

    adding constants

---
+++ HalfArcLineExpanderStage.ts
@@ -0,0 +1,8 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const nodes : number = 5
+const lines : number = 2
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const sizeFactor : number = 3
+const strokeFactor : number = 90
