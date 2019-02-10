53db72c286b9acc5a10d0f32d60a6d2e909ad48f
Anwesh43 <anweshthecool0@gmail.com>
2019-02-11 01:28:38 +0530

    adding HalfArcLineExpander class

--- HalfArcLineExpanderStage.ts
+++ HalfArcLineExpanderStage.ts
@@ -188,3 +188,27 @@ class HALENode {
         return this
     }
 }
+
+class HalfArcLineExpander {
+    root : HALENode = new HALENode(0)
+    curr : HALENode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
